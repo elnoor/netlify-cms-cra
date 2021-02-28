266e74666354f633ebe50e7a7963427c7d39e0e7
elnoor <elnoormobile@gmail.com>
2021-02-28 04:08:02 -0700

    marked down installed; single post ui changes

--- src/pages/BlogPosts.js
+++ src/pages/BlogPosts.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import marked from "marked";
 
 const blogpostsFolder = "contents/blogposts/";
 
 export default function BlogPosts(props) {
-  const [posts, setPosts] = useState([]);
+  const [postFileNames, setPosts] = useState([]);
+  const [currentPost, setCurrentPost] = useState(null);
 
   useEffect(() => {
     fetch(blogpostsFolder + "index.json")
@@ -13,16 +15,26 @@ export default function BlogPosts(props) {
       });
   }, []);
 
-  return (
-    <div className="blog">
-      <h2>Blog</h2>
-      <div className="posts">
-        {posts.map((post, index) => {
-          return <PostTile key={index} fileName={post} />;
-        })}
+  if (currentPost) {
+    return <Post fileName={currentPost} setCurrentPost={setCurrentPost} />;
+  } else {
+    return (
+      <div className="blog">
+        <h2>Blog</h2>
+        <div className="posts">
+          {postFileNames.map((post, index) => {
+            return (
+              <PostTile
+                key={index}
+                fileName={post}
+                setCurrentPost={setCurrentPost}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function PostTile(props) {
@@ -37,12 +49,44 @@ function PostTile(props) {
   }, [props.fileName]);
 
   return (
-    <div className="post-tile">
+    <div
+      className="post-tile"
+      onClick={() => props.setCurrentPost(props.fileName)}
+    >
       <img src={post.thumbnail} alt="thumbnail" />
       <div className="details">
         <div className="title">{post.title}</div>
         <div className="date">{new Date(post.date).toDateString()}</div>
       </div>
     </div>
+  );
+}
+
+function Post(props) {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    if (!post) {
+      fetch(blogpostsFolder + props.fileName)
+        .then((response) => response.json())
+        .then((data) => {
+          setPost(data);
+        });
+    }
+  }, [props.fileName, post]);
+
+  return (
+    post && (
+      <div className="post">
+        <a onClick={() => props.setCurrentPost(null)} href="##">
+          BACK
+        </a>
+        <h2>{post.title}</h2>
+        <img src={post.thumbnail} alt="thumbnail" />
+        <article
+          dangerouslySetInnerHTML={{ __html: marked(post.body) }}
+        ></article>
+      </div>
+    )
   );
 }
