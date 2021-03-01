e3cc8f00ef273d204c0a66ed441b52c8713c6649
elnoor <elnoormobile@gmail.com>
2021-03-01 00:12:01 -0700

    admin/index.html disabled

--- public/admin/index.js
+++
@@ -1,6 +0,0 @@
-import CMS from "netlify-cms-app";
-import { Post } from "../../src/pages/BlogPosts.js";
-
-// CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("blog", Post);
-CMS.init();
