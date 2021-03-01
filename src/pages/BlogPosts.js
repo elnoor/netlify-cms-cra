7eac6e86c1b773d534aa498eeaeb93498a0311a8
elnoor <elnoormobile@gmail.com>
2021-02-28 18:57:59 -0700

    blogpost react page changes

--- src/pages/BlogPosts.js
+++ src/pages/BlogPosts.js
@@ -3,21 +3,23 @@ import marked from "marked";
 
 const blogpostsFolder = "contents/blogposts/";
 
-export default function BlogPosts(props) {
-  const [postFileNames, setPosts] = useState([]);
+export default function BlogPosts() {
+  const [postFileNames, setPostFileNames] = useState([]);
   const [currentPost, setCurrentPost] = useState(null);
 
   useEffect(() => {
     fetch(blogpostsFolder + "index.json")
       .then((response) => response.json())
       .then((data) => {
-        setPosts(data);
+        setPostFileNames(data);
       });
   }, []);
 
   if (currentPost) {
-    return <Post fileName={currentPost} setCurrentPost={setCurrentPost} />;
-  } else {
+    return (
+      <Post post={currentPost} clearCurrentPost={() => setCurrentPost(null)} />
+    );
+  } else if (postFileNames && postFileNames.length > 0) {
     return (
       <div className="blog">
         <h2>Blog</h2>
@@ -34,6 +36,8 @@ export default function BlogPosts(props) {
         </div>
       </div>
     );
+  } else {
+    return <p>No posts found</p>;
   }
 }
 
@@ -49,10 +53,7 @@ function PostTile(props) {
   }, [props.fileName]);
 
   return (
-    <div
-      className="post-tile"
-      onClick={() => props.setCurrentPost(props.fileName)}
-    >
+    <div className="post-tile" onClick={() => props.setCurrentPost(post)}>
       <img src={post.thumbnail} alt="thumbnail" />
       <div className="details">
         <div className="title">{post.title}</div>
@@ -63,22 +64,11 @@ function PostTile(props) {
 }
 
 function Post(props) {
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    if (!post) {
-      fetch(blogpostsFolder + props.fileName)
-        .then((response) => response.json())
-        .then((data) => {
-          setPost(data);
-        });
-    }
-  }, [props.fileName, post]);
-
+  const { post } = props;
   return (
     post && (
       <div className="post">
-        <button onClick={() => props.setCurrentPost(null)} href="##">
+        <button onClick={props.clearCurrentPost}>
           BACK
         </button>
         <h2>{post.title}</h2>
